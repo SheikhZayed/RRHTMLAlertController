a1afcc57517c61e84192a779c35ccf9e67d4fa8c
Rodrigo Rallo <rodrallo@gmail.com>
2016-03-16 16:38:59 -0700

    -added controller to showcase example of preloaded html alert

--- RRHTMLAlertController/PreloadedViewController.swift
+++ RRHTMLAlertController/PreloadedViewController.swift
@@ -12,16 +12,29 @@ class PreloadedViewController: UIViewController {
 
     @IBOutlet weak var showButton: UIButton!
     
+    private let htmlAlertController : RRHTMLViewController = RRHTMLViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        //You could write and preload your own html file into your app bundle to show these alerts immediately:
+        let htmlFile = NSBundle.mainBundle().pathForResource("CustomHTMLView", ofType: "html")
+        if let htmlString = try? String(contentsOfFile: htmlFile!, encoding: NSUTF8StringEncoding){
+            self.htmlAlertController.htmlString = htmlString
+        }
 
+        self.htmlAlertController.showWhenReady = false;
+        self.htmlAlertController.enableAnimation = true;
+        self.htmlAlertController.enableDismissOnBackgroundTap = false;
+        self.htmlAlertController.closeURLString = "yourcompany://close"
+        
+    }
+    
+    @IBAction func showButtonTapped(sender: AnyObject) {
+        self.presentViewController(self.htmlAlertController, animated: false, completion: nil)
+    }
 
 }
 
