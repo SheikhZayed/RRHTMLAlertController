e863e0f61e22e33c0fb9ede92231c0f2b772ffc3
Rodrigo Rallo <rodrallo@gmail.com>
2016-03-16 14:53:41 -0700

    -added tabview controller to showcase the two ways in which the view controller can be created

--- RRHTMLAlertController/LazyViewController.swift
+++ RRHTMLAlertController/LazyViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LazyViewController.swift
 //  RRHTMLAlertController
 //
 //  Created by Rodrigo Rallo on 3/16/16.
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LazyViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
---
+++ RRHTMLAlertController/PreloadedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RRHTMLAlertController
+//
+//  Created by Rodrigo Rallo on 3/16/16.
+//  Copyright © 2016 Rallo. All rights reserved.
+//
+
+import UIKit
+
+class PreloadedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
